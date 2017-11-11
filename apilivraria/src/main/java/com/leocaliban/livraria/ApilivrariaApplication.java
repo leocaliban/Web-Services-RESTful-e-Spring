65f71b4a9c07fb23d6c6ed5440de13097b1f0eb1
Leocaliban <leocaliban@gmail.com>
2017-11-11 02:08:12 -0300

    Início

---
+++ apilivraria/src/main/java/com/leocaliban/livraria/ApilivrariaApplication.java
@@ -0,0 +1,12 @@
+package com.leocaliban.livraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApilivrariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApilivrariaApplication.class, args);
+	}
+}
