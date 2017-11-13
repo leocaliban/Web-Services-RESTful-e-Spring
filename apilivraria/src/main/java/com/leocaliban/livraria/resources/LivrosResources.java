4c6ce5833380c344ffd758ccd2ade59fbcb7786d
Leocaliban <leocaliban@gmail.com>
2017-11-13 02:14:36 -0300

    2.6. Salvando o recurso Livro a partir de um POST

--- apilivraria/src/main/java/com/leocaliban/livraria/resources/LivrosResources.java
+++ apilivraria/src/main/java/com/leocaliban/livraria/resources/LivrosResources.java
@@ -3,6 +3,7 @@ package com.leocaliban.livraria.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,18 @@ import com.leocaliban.livraria.domain.Livro;
 import com.leocaliban.livraria.repository.LivrosRepository;
 
 @RestController
+@RequestMapping("/livros")
 public class LivrosResources {
 	@Autowired
 	private LivrosRepository livroRepository;
 
-	@RequestMapping(value = "/livros", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public List<Livro> listar() {
 		return livroRepository.findAll();
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public void salvar(@RequestBody Livro livro) {
+		livroRepository.save(livro);
 	}
 }
