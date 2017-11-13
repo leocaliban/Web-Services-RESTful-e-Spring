081faffe1b50d75b1b07b2d27f137607f16d4605
Leocaliban <leocaliban@gmail.com>
2017-11-13 02:20:20 -0300

    2.7. Buscando um livro com o uso da anotação @PathVariable

--- apilivraria/src/main/java/com/leocaliban/livraria/resources/LivrosResources.java
+++ apilivraria/src/main/java/com/leocaliban/livraria/resources/LivrosResources.java
@@ -3,6 +3,7 @@ package com.leocaliban.livraria.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,5 +26,10 @@ public class LivrosResources {
 	@RequestMapping(method = RequestMethod.POST)
 	public void salvar(@RequestBody Livro livro) {
 		livroRepository.save(livro);
+	}
+	
+	@RequestMapping(value = "/{id}",method = RequestMethod.GET)
+	public Livro buscar(@PathVariable("id") Long id) {
+		return livroRepository.findOne(id);
 	}
 }
