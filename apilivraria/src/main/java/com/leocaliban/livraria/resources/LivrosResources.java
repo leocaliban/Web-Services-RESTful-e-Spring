1d4bf55e9fd188b8e3f373b16987fbbfe9057604
Leocaliban <leocaliban@gmail.com>
2017-11-16 03:56:44 -0300

    2.10. Tratamento correto das respostas HTTP 404 e 201

--- apilivraria/src/main/java/com/leocaliban/livraria/resources/LivrosResources.java
+++ apilivraria/src/main/java/com/leocaliban/livraria/resources/LivrosResources.java
@@ -1,13 +1,17 @@
 package com.leocaliban.livraria.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.leocaliban.livraria.domain.Livro;
 import com.leocaliban.livraria.repository.LivrosRepository;
@@ -24,13 +28,22 @@ public class LivrosResources {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public void salvar(@RequestBody Livro livro) {
-		livroRepository.save(livro);
+	public ResponseEntity<Void> salvar(@RequestBody Livro livro) {
+		//tratando o retorno http quando o conteúdo é salvo
+		livro = livroRepository.save(livro);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(livro.getIdLivro()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}",method = RequestMethod.GET)
-	public Livro buscar(@PathVariable("id") Long id) {
-		return livroRepository.findOne(id);
+	public ResponseEntity<?> buscar(@PathVariable("id") Long id) {
+		Livro livro = livroRepository.findOne(id);
+		//tratando o retorno http quando o conteúdo da requisição não existe
+		if(livro == null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(livro);
 	}
 	
 	@RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
